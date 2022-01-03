94f4e34b99b6d1ffdce155764fcaf25c681b9107
aceto <aceto@DESKTOP-5R3PJ0J.home>
2022-01-03 19:05:42 +0100

    dio

--- macchinariestetici/src/macchinariestetici/App.java
+++ macchinariestetici/src/macchinariestetici/App.java
@@ -1,14 +1,86 @@
 package macchinariestetici;
+import java.sql.*;
+import java.util.*;
 
 public class App {
 
-	public App() {
-		//carlo santonicola-a-a
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/progetto"
+			+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true"
+			+ "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+			String username = "root"; 
+			String pwd = "30061984";
+			con = DriverManager.getConnection(url,username,pwd);
+			
+			System.out.println("Connessione riuscita");
+			
+			Scanner in = new Scanner(System.in);
+			
+			int querySelector = 1;
+			
+			switch (querySelector) {
+			case 0:
+				try {
+					Statement query = con.createStatement();
+					ResultSet result = query.executeQuery("SELECT cf FROM cliente");
+					
+					while(result.next()) {
+						String cf = result.getString("cf");
+						System.out.println(cf);
+					}
+				} catch (Exception e) {
+					System.out.println("Errore nella Query #0");
+				}
+				
+				break;
+				
+			case 1:
+				try {
+					System.out.println("Inserire nome cliente: ");
+					String nome = in.nextLine();
+					
+					System.out.println("Inserire cognome cliente: ");
+					String cognome = in.nextLine();
+					
+					System.out.println("Inserire codice fiscale cliente: ");
+					String cf = in.nextLine();
+					
+					System.out.println("Inserire un recapito telefonico: ");
+					String tel = in.nextLine();
+					
+					System.out.println("Inserire indirizzo email: ");
+					String email = in.nextLine();
+					
+					System.out.println("Inserire numero acquisti effettuati: ");
+					int numAcquisti = in.nextInt();
+					
+					Statement query1 = con.createStatement();
+					query1.executeQuery("INSERT INTO cliente(nome,cognome,cf,numAcquisti,telefono,email) VALUES('"+nome+"','"+cognome+"','"+cf+"','"+numAcquisti+"','"+tel+"','"+email+"')");
+					
+					System.out.println("Query #1 eseguita con successo");
+				} catch (Exception e) {
+					System.out.println("Errore nell'interrogazione della Query #1!");
+				}
+				
+				break;
 
+			default:
+				break;
+			}
+			
+			
+			
+			
+			
+		}catch(Exception e){System.out.println("Connessione fallita!!!");}
+		
+		
+		
 	}
 
 }
